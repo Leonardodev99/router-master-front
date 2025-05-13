7e16258349a9565dea3843e737eb826e65212e48
Leonardo Tiago <pequenotiago@gmail.com>
2025-05-13 06:14:54 +0100

    Integration testing

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,24 +1,43 @@
 import React, { useState } from "react";
 import "../styles/LoginScreen.css";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+
 
 function LoginScreen() {
   const navigate = useNavigate(); // Usando React Router para navegação
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
 
-  const handleLogin = (e) => {
+  const apiUrl = 'http://localhost:3005';
+
+  
+  const handleLogin = async (e) => {
     e.preventDefault();
-    if (!email && !password) {
-      setError('Preencha todos os campos!')
-      return;
-    }
+    try {
 
-    if(email === 'tiago@gmail.com' && password === "1234") {
-      navigate("/home");
-    } else {
-      setError("E-mail ou password incorretos!");
+      const response = await axios.post(`${apiUrl}/tokens`, {
+        email,
+        password,
+      });
+
+      if(response.status === 200) {
+        const token = response.data.token;
+
+        localStorage.setItem('token', token);
+        console.log("Token enviado na requisição:", token);
+
+        setMessage('Login sucessfull!');
+        navigate("/home");
+      } else {
+        setMessage('Invalid email or password');
+      }
+
+    } catch (error) {
+      console.error('Error login:', error);
+      setMessage('Erro ao realizar login. Tente novamente mais tarde.');
     }
   };
 
